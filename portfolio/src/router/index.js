e976eb7a00ad8566da66b2b0b43aa5820f1d0dd1
Yoosup Song <Youssupsong@gmail.com>
2024-11-21 20:05:15 -0500

    finished website

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'home',
       component: () => import('../views/Home.vue'),
     },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/Contact.vue'),
+    },
   ]
 })
 
--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,10 +1,25 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <h2 class="text-teal-500	">hello</h2>
-    <button class="btn btn-primary">Primary</button>
+    <div class="hero bg-base-700 min-h-screen">
+        <div class="hero-content flex-col lg:flex-row">
+            <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                class="max-w-lg rounded-sm shadow-sm" />
+            <div>
+                <h1 class="text-5xl font-bold">Yoosup</h1>
+                <p class="py-6">
+                    Yoosup is a platform that allows you to connect with your friends and family. You can chat, call,
+                    and
+                    video call with your loved ones.
+                </p>
+                <RouterLink to="/contact">
+                    <button class="btn btn-primary">contact me</button>
+                </RouterLink>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
